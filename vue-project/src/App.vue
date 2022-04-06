195ffd8bec2d272bfd0c0b448c38ed23e5f0a1ef
Jurjen <jurjen.veenstra@hotmail.nl>
2022-04-06 16:27:44 +0200

    Comments toegevoegd
    Er misten comments in het document. en voorgesteld om script, html en css code apart in bestanden te zetten

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,3 +1,4 @@
+<!--In dit document zit scirpt code html en css code. het is overzichtelijker als je het in aparte bestanden zet-->
 <script>
   export default {
     data() {
@@ -8,24 +9,28 @@
       }
     },
     created() {
+      //api call uitvoeren
       this.fetchData()
     },
+    //Api call om pokemons op te halen
     methods: {
       fetchData() {
         this.error = this.post = null
         this.loading = true
         fetch('https://pokeapi.co/api/v2/pokemon?limit=151').then(res => res.json()).then(data => {
           this.loading = false;
+          //variabele van alle pokemons
           this.pokemon = data.results;
         })
       },
     },
   }
 </script>
-
 <template>
   <div v-if="loading">LOADING</div>
   <div class="pokemon-grid">
+<!--    alle opgehaalde pokemons laten zien-->
+<!--    pokemon variabele word gebruikt van functie fetchData()-->
     <div class="pokemoncard" v-for="pokemon in pokemon" :key="pokemon.name">
       {{ pokemon.name }}
     </div>
@@ -33,6 +38,7 @@
 </template>
 
 <style>
+/*style van grid om pokemons te tonen*/
 .pokemon-grid {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
